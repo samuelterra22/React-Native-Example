fd9d80943241e4189f33dca19890cf7c54f29d50
Samuel Terra <samuelterra22@hotmail.com>
2019-08-06 11:11:15 -0300

    Initial commit

---
+++ src/pages/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import { View, Text } from 'react-native'
+
+export default class Main extends Component {
+  // Método obrigatório
+  render() {
+    return (
+      <View>
+        <Text>Main Page</Text>
+      </View>
+    )
+  }
+}
