45674d718dbff50f28d0114e4cfbe0caaa0ba891
Samuel Terra <samuelterra22@hotmail.com>
2019-08-08 12:13:56 -0300

    Improve code

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: "JSHunt"
+  }
   // Método obrigatório
   render() {
     return (
       <View>
-        <Text>Main Page</Text>
+        <Text>Main Page.</Text>
       </View>
     )
   }
